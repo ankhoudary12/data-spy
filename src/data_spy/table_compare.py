3566fbe34a72ed04a43e30bb2d9aea06541d0f9d
Anthony Khoudary <ankhoudary12@gmail.com>
2022-11-15 12:02:43 -0500

    add getters/setters for pkey and raw diff

--- src/data_spy/table_compare.py
+++ src/data_spy/table_compare.py
@@ -4,6 +4,8 @@ import logging
 
 from decouple import config
 
+import jinja2
+
 import pandas as pd
 
 from snowflake_utils import snowflake_ctx
@@ -17,12 +19,14 @@ model_meta_table = config("MODEL_META_TABLE")
 
 
 class table_compare:
-    """Class for comparing two Snowflake tables and materializing diff in the warehouse."""
+    """Class for comparing two Snowflake tables and materializing diff in the warehouse.
+    Assumes this is a dbt implementation where dev/prod tables are the same name, different schemas.
+    """
 
-    def __init__(self, dev_table: str, prod_table: str) -> None:
+    def __init__(self, table: str) -> None:
         """Initialize with table names."""
-        self.dev_table = dev_table
-        self.prod_table = prod_table
+        self.dev_table = table
+        self.prod_table = table
 
         self.dev_database = "analytics"
         self.dev_schema = "dev_akhoudary"
@@ -31,6 +35,13 @@ class table_compare:
         self.model_meta_table = model_meta_table
 
         self.sf_ctx = snowflake_ctx()
+
+        # set primary/freshness keys for each table
+        self.dev_pkey = self.set_primary_key(self.dev_table)
+        self.prod_pkey = self.set_primary_key(self.prod_table)
+
+        self.dev_freshness_key = self.set_freshness_key(self.dev_table)
+        self.prod_freshness_key = self.set_freshness_key(self.prod_table)
 
     def _create_sf_ctx(self) -> None:
         """Create a snowflake cursor object."""
@@ -92,18 +103,65 @@ class table_compare:
 
         return self.sf_ctx.execute_sql_and_return_single_value(sql)
 
-    def summary_diff(self) -> None:
-        """Runs a high level diff on the dev/prod tables and materializes output in dev database."""
-        # get primary keys for each table
-        dev_pkey = self._get_primary_key(self.dev_table)
-        print(f"Dev key: {dev_pkey}")
-        prod_pkey = self._get_primary_key(self.prod_table)
-        print(f"Prod key: {prod_pkey}")
-        # get freshness keys for each table.
-        dev_freshness_key = self._get_freshness_key(self.dev_table)
-        print(f"Dev freshness key: {dev_freshness_key}")
-        prod_freshness_key = self._get_freshness_key(self.prod_table)
-        print(f"Prod freshness key: {prod_freshness_key}")
+    def set_primary_key(self, table: str) -> str:
+        """Sets internal params for primary key.
+
+        Args:
+            table: table you want to retrieve pkey for.
+
+        Returns:
+            str: pkey for the table.
+        """
+
+        pkey = self._get_primary_key(table)
+        return pkey
+
+    def set_freshness_key(self, table: str) -> str:
+        """Sets internal param for freshness key.
+
+        Args:
+            table: table you want to retrieve freshness key for.
+
+        Returns:
+            str: freshness key for the table.
+        """
+
+        freshness_key = self._get_freshness_key(table)
+        return freshness_key
+
+    def get_columns_in_table(self, database: str, schema: str, table: str) -> list:
+        """Gets list of column names for a table.
+
+        Args:
+            database: name of database where information schema lives.
+            schema: name of schema of table.
+            table: name of table to query.
+
+        Returns:
+            list: list of column names for that table.
+        """
+
+        sql = f"""
+            SELECT
+                column_name
+            FROM {database}.information_schema.columns
+            WHERE LOWER(table_schema) = '{schema}'
+            AND LOWER(table_name) = '{table}'
+        """
+        # get column names as a df and convert to list
+        column_df = self.sf_ctx.sql_to_df(sql)
+        columns = column_df["COLUMN_NAME"].values.tolist()
+
+        return columns
+
+    def summary_diff(self) -> pd.DataFrame:
+        """Runs a high level diff on the dev/prod tables and materializes output in dev database.
+
+        Args:
+            None
+
+        Returns: df: DataFrame object representing the summary diff.
+        """
 
         sql = f"""
         
@@ -111,29 +169,104 @@ class table_compare:
             SELECT
                 *
             FROM { self.dev_database}.{ self.dev_schema }.{ self.dev_table }
-            WHERE { dev_freshness_key } < CURRENT_DATE()
-        ),
+            WHERE { self.dev_freshness_key } < CURRENT_DATE()
 
+        ),
         prod AS (
             SELECT
                 *
             FROM { self.prod_database}.{ self.prod_schema }.{ self.prod_table }
-            WHERE { prod_freshness_key } < CURRENT_DATE()
+            WHERE { self.prod_freshness_key } < CURRENT_DATE()
         )
 
         SELECT
             '{ self.dev_table }' AS table_name,
             (SELECT COUNT(*) FROM dev ) AS rowcount_dev,
             (SELECT COUNT(*) FROM prod ) AS rowcount_prod,
+            (rowcount_dev - rowcount_prod) / rowcount_prod AS rowcount_diff,
             (SELECT COUNT(*) FROM { self.dev_database }.information_schema.columns WHERE LOWER(table_schema) = '{ self.dev_schema }' AND LOWER(table_name) = '{ self.dev_table }') AS column_count_dev,
             (SELECT COUNT(*) FROM { self.prod_database }.information_schema.columns WHERE LOWER(table_schema) = '{ self.prod_schema }' AND LOWER(table_name) = '{ self.prod_table }') AS column_count_prod,
-            (SELECT COUNT(DISTINCT { dev_pkey }) FROM dev ) AS distinct_pkey_dev,
-            (SELECT COUNT(DISTINCT { prod_pkey }) FROM prod ) AS distinct_pkey_prod 
+            (column_count_dev - column_count_prod) / column_count_prod AS column_count_diff,
+            (SELECT COUNT(DISTINCT { self.dev_pkey }) FROM dev ) AS distinct_pkey_dev,
+            (SELECT COUNT(DISTINCT { self.prod_pkey }) FROM prod ) AS distinct_pkey_prod,
+            (distinct_pkey_dev - distinct_pkey_prod) / distinct_pkey_prod AS distinct_pkey_diff
             ;
         """
 
-        df = self.sf_ctx.sql_to_df(sql)
+        return self.sf_ctx.sql_to_df(sql)
 
-        self.sf_ctx.write_df_to_snowflake(
-            df, self.dev_database, self.dev_schema, f"{self.dev_table}_summary_diff"
+    def row_level_diff(self) -> None:
+        """Executes a row level diff of the two tables and outputs a new table in the dev schema."""
+
+        dev_columns = self.get_columns_in_table(
+            self.dev_database, self.dev_schema, self.dev_table
         )
+
+        prod_columns = self.get_columns_in_table(
+            self.prod_database, self.prod_schema, self.prod_table
+        )
+
+        # we want to materialize this output all intra-database since it could be a large dataset
+        # using jinja2 formatting to prefix all column names with dev/prod to avoid duplicate column error
+        sql = """
+        CREATE OR REPLACE TABLE {{ dev_database }}.{{ dev_schema }}.{{ dev_table }}_raw_diff AS (
+
+            WITH dev AS (
+                SELECT
+                    {{ dev_pkey }} AS dev_primary_key,
+                    {% for column in dev_columns %}
+                    {{ column }} AS dev_{{ column }} {% if not loop.last %},{% endif %}
+                    {% endfor %}
+                FROM {{ dev_database }}.{{ dev_schema }}.{{ dev_table }}
+                WHERE {{ dev_freshness_key }} < CURRENT_DATE()
+            ),
+
+            prod AS (
+                SELECT
+                    {{ prod_pkey }} AS prod_primary_key,
+                    {% for column in prod_columns %}
+                    {{ column }} AS prod_{{ column }} {% if not loop.last %},{% endif %}
+                    {% endfor %}
+                FROM {{ prod_database }}.{{ prod_schema }}.{{ prod_table }}
+                WHERE {{ prod_freshness_key }} < CURRENT_DATE()
+            ),
+
+            joined AS (
+                SELECT
+                    CASE 
+                        WHEN a.dev_primary_key IS NOT NULL AND b.prod_primary_key IS NULL THEN 'addition'
+                        WHEN a.dev_primary_key IS NULL AND b.prod_primary_key IS NOT NULL THEN 'deletion'
+                    ELSE 'equal' END AS diff_type,
+                    a.*,
+                    b.*
+                FROM dev a
+                FULL OUTER JOIN prod b ON a.dev_primary_key = b.prod_primary_key
+            )
+
+            SELECT
+                *
+            FROM joined
+            WHERE diff_type != 'equal'
+            );
+        """
+
+        context = {
+            "dev_database": self.dev_database,
+            "dev_schema": self.dev_schema,
+            "dev_table": self.dev_table,
+            "dev_pkey": self.dev_pkey,
+            "dev_freshness_key": self.dev_freshness_key,
+            "dev_columns": dev_columns,
+            "prod_database": self.prod_database,
+            "prod_schema": self.prod_schema,
+            "prod_table": self.prod_table,
+            "prod_pkey": self.prod_pkey,
+            "prod_freshness_key": self.prod_freshness_key,
+            "prod_columns": prod_columns,
+        }
+
+        environment = jinja2.Environment()
+        template = environment.from_string(sql)
+        sql_formatted = template.render(context)
+        print(sql_formatted)
+        self.sf_ctx.execute_sql(sql_formatted)
